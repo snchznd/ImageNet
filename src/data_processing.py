876d3e136486efca695ee1ac3375689c23767124
Miguel <miguel.ngl.sanchez@gmail.com>
2024-08-09 01:01:55 +0200

    started implementing data_processing.py

---
+++ src/data_processing.py
@@ -0,0 +1,25 @@
+from src.data_loading import load_dataset
+from src.image_net_dataset import ImageNetDataset
+from torch.utils.data import DataLoader
+
+def get_data_loaders(train_batch_size,
+                     val_batch_size,
+                     shuffle_train,
+                     shuffle_val,
+                     train_transforms = None,
+                     val_transforms = None,
+                     num_workers=32
+                     ):
+    data_splits = load_dataset()
+    train_dataset = ImageNetDataset(data_splits['train'], train_transforms)
+    val_dataset = ImageNetDataset(data_splits['val'], val_transforms)
+    train_data_loader = DataLoader(dataset=train_dataset,
+                                   batch_size=train_batch_size,
+                                   shuffle=shuffle_train,
+                                   num_workers=num_workers)
+    val_data_loader = DataLoader(dataset=val_dataset,
+                                   batch_size=val_batch_size,
+                                   shuffle=shuffle_val,
+                                   num_workers=num_workers)
+
+    return train_data_loader, val_data_loader
