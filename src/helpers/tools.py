965b62e652b3162d86ad40846289b866ee450dd5
Miguel <miguel.ngl.sanchez@gmail.com>
2024-08-31 14:21:00 +0200

    added tools to compute the dimension of the output of a series a convolutional layers

--- src/helpers/tools.py
+++ src/helpers/tools.py
@@ -6,7 +6,16 @@ import torch
 
 DEFAULT_EXP_PATH = '/home/masn/projects/ImageNet/experiments'
 
-def compute_conv_output_size(h_in, w_in, kernel_size, stride, padding, dilation):
+def compute_conv_output_size(h_in : int,
+                             w_in : int,
+                             kernel_size : Tuple[int, int],
+                             stride : Tuple[int, int],
+                             padding : Tuple[int, int],
+                             dilation : Tuple[int, int]):
+    '''
+    note that this computation is valid both for the output size of a 
+    convolutional layer or for that of a max pooling layer.
+    '''
     def numerator(dim_in, idx):
         return dim_in + 2 * padding[idx] - dilation[idx] * (kernel_size[idx] - 1) - 1
 
@@ -17,7 +26,46 @@ def compute_conv_output_size(h_in, w_in, kernel_size, stride, padding, dilation)
     for dim_name, dim_in, idx in [('H_out', h_in, 0), ('W_out', w_in, 1)]:
         expr = numerator(dim_in, idx) / denominator(idx) + 1
         out_dims[dim_name] = math.floor(expr)
-    return out_dims
+    return out_dims['H_out'], out_dims['W_out']
+
+def get_conv_output_shape(
+        input_height: int,
+        input_width: int,
+        nbr_layers : int,
+        using_max_pooling : bool,
+        convolutions_kernel_size : Tuple[int, int],
+        convolutions_stride : Tuple[int, int],
+        convolutions_padding : Tuple[int, int],
+        convolutions_dilation : Tuple[int, int],
+        max_pooling_kernel_size : Tuple[int, int],
+        max_pooling_stride : Tuple[int, int],
+        max_pooling_padding : Tuple[int, int] = (0,0),
+        max_pooling_dilation : Tuple[int, int] = (1,1)
+                     ):
+    current_height = input_height
+    current_width = input_width
+    for _ in range (nbr_layers):
+        current_height, current_width = compute_conv_output_size(
+            h_in=current_height,
+            w_in=current_width,
+            kernel_size=convolutions_kernel_size,
+            stride=convolutions_stride,
+            padding=convolutions_padding,
+            dilation=convolutions_dilation,
+            ) 
+        
+        if using_max_pooling:
+            current_height, current_width = compute_conv_output_size(
+                h_in=current_height,
+                w_in=current_width,
+                kernel_size=max_pooling_kernel_size,
+                stride=max_pooling_stride,
+                padding=max_pooling_padding,
+                dilation=max_pooling_dilation,
+                ) 
+    1
+    return current_height, current_width
+    
 
 
 def compute_model_nbr_params(model: torch.nn.Module) -> int:
@@ -62,3 +110,23 @@ def initialize_experiment_directories(experiment_name : str
     for path in logs_dir_path, models_dir_path:
         os.makedirs(path)
     return logs_dir_path, models_dir_path
+
+def main():
+    dim = \
+    get_conv_output_shape(
+        input_height=256,
+        input_width=256,
+        nbr_layers=5,
+        using_max_pooling=True,
+        convolutions_kernel_size=(3,3),
+        convolutions_stride=(1,1),
+        convolutions_padding=(1,1),
+        convolutions_dilation=(1,1),
+        max_pooling_kernel_size=(2,2),
+        max_pooling_stride=(2,2),
+        )
+
+    print(f'    ---> {dim}')
+
+if __name__ == '__main__':
+    main()
